5caa159ed732325d74f56cb09b019a9674eb6375
Jonathan Philipps <38406134+jphilipps@users.noreply.github.com>
2023-02-23 21:17:31 +0000

    🎨 code structure improvements

--- src/react.ts
+++ src/react.ts
@@ -7,7 +7,9 @@ import { render } from "react-dom";
 export type ReactView<Model, Msg> = View<Model, Msg, ReactElement>;
 
 export const withReactSynchronous = (parent: HTMLElement) =>
-    <Arg, Model, Msg>(prog: Program<Arg, Model, Msg, ReactElement>) => pipe(
+    <Arg, Model, Msg>(
+        prog: Program<Arg, Model, Msg, ReactElement>
+    ) => pipe(
         prog,
         program.withSetState<Model, Msg>((dispatch, model) => {
             render(prog.view(dispatch, model), parent);
