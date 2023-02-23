2f3da0f02db5422c910c0c2b8a2863a2fd7a26d7
Jonathan Philipps <38406134+jphilipps@users.noreply.github.com>
2023-02-23 00:29:32 +0000

    ✨ migrate repo from fun-ts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fun-ts-elmish-react-legacy
+
+*Implementation of the elm architecture with typescript and a react legacy wrapper because some frameworks like ionic doesn't support react 18 yet.*
---
+++ src/react.ts
@@ -0,0 +1,15 @@
+import { Program, View, program } from "fun-ts-elmish";
+
+import { ReactElement } from "react";
+import { pipe } from "fp-ts/function";
+import { render } from "react-dom";
+
+export type ReactView<Model, Msg> = View<Model, Msg, ReactElement>;
+
+export const withReactSynchronous = (parent: HTMLElement) =>
+    <Arg, Model, Msg>(prog: Program<Arg, Model, Msg, ReactElement>) => pipe(
+        prog,
+        program.withSetState<Model, Msg>((dispatch, model) => {
+            render(prog.view(dispatch, model), parent);
+        }),
+    );
